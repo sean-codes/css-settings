fcb3fbc8bf4447706fd3a23284877af724387fe8
sean-codes <sean_codes@outlook.com>
2018-02-10 19:51:18 -0600

    reacting the input/change

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 function CSSettings(options) {
    this.construct = function(options) {
       this.container = document.createElement('div')
-      this.container.classList.add('cssettings-container')
+      this.container.classList.add('cssettings')
       document.body.appendChild(this.container)
 
       this.settings = []
@@ -14,35 +14,43 @@ function CSSettings(options) {
    this.setup = function(setting) {
       // Create
       var container = document.createElement('div')
-      container.classList.add('cssettings-setting-container')
+      container.classList.add('cssettings-setting')
       setting.html = {
          label: document.createElement('div'),
-         input: document.createElement('input'),
+         input: document.createElement(setting.input.type == 'select' ? 'select' : 'input'),
          value: document.createElement('div')
       }
       setting.html.label.classList.add('cssettings-setting-label')
       setting.html.input.classList.add('cssettings-setting-input')
       setting.html.value.classList.add('cssettings-setting-value')
-
+      if(setting.input.type == 'select') {
+         for(var option of setting.options) {
+            setting.html.input.innerHTML += '<option>'+option+'</option'
+         }
+      }
       // Append
       this.container.appendChild(container)
       for(var element in setting.html) container.appendChild(setting.html[element])
 
       // Config
+      setting.suffix = setting.suffix || ''
       for(property in setting.input) setting.html.input[property] = setting.input[property]
       setting.html.value.innerHTML = setting.html.input.value + setting.suffix
       setting.html.label.innerHTML = setting.label
-
       // Listeners
       setting.html.input.addEventListener('change', this.change.bind(setting))
+      setting.html.input.addEventListener('input', this.change.bind(setting))
 
       return
    }
 
    this.change = function() {
-      console.log(this)
-      var value = this.html.input.value + this.suffix
-      this.html.value.innerHTML = value
+      console.log('change')
+      var value = this.html.input.value
+      if(this.input.type == 'checkbox'){
+         value = this.html.input.checked ? this.html.input.value : 'initial'
+      }
+      this.html.value.innerHTML = value + this.suffix;
    }
 
    this.construct(options)
