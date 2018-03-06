e8dd9ed31873b7b436f85e3930208b250d9acb5f
sean_codes <sean_codes@outlook.com>
2018-03-06 10:00:35 -0600

    drop the stylesheet in javascript for less includes

--- dist/CSSSettings.js
+++ dist/CSSSettings.js
@@ -13,6 +13,60 @@ function CSSettings(options) {
 
    this.setup = function(setting) {
       // Create
+		var styles = document.createElement('style')
+		document.head.appendChild(styles)
+		styles.innerHTML = `
+			.cssettings {
+				position:fixed;
+				top:0; left:0;
+				color:#FFF;
+				background:rgba(0, 0, 0, 0.75);
+				padding:0px 10px 10px;
+				font-size:12px;
+				font-weight:600;
+				font-family:monospace;
+				width:auto;
+				display:inline-block;
+			}
+
+			.cssettings .cssettings-setting {
+				display:table-row;
+			}
+
+			.cssettings .cssettings-setting .cssettings-setting-label,
+			.cssettings .cssettings-setting .cssettings-setting-input,
+			.cssettings .cssettings-setting .cssettings-setting-value{
+				font-family:monospace;
+				display:table-cell;
+				white-space:nowrap;
+				padding-top:5px;
+				padding-right:5px;
+			}
+
+			.cssettings .cssettings-setting .cssettings-setting-input input,
+			.cssettings .cssettings-setting .cssettings-setting-input select{
+				box-sizing:border-box;
+				border:none;
+				border-radius:0px;
+				width:100%;
+				font-size:inherit;
+			}
+
+			.cssettings .cssettings-setting .cssettings-setting-input select{
+				-webkit-appearance: none;
+				border-radius: 0px;
+				position:relative;
+			}
+
+			.cssettings .cssettings-setting[type='select']:after{
+				content: ' ';
+				border:10px solid transparent;
+				border-top:10px solid #FFF;
+				left:5px; top:5px;
+				position:absolute;
+			}
+		`;
+
       var container = document.createElement('div')
       container.classList.add('cssettings-setting')
       setting.html = {
